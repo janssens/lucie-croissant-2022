5319851008a58c24852b8ad31f190a1745cd29ae
janssens <janssensgaetan@gmail.com>
2022-03-18 22:29:53 +0100

    a good start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'union-blue': '#3F2682',
+        'eelv-green': '#409682'
+      },
+    },
+  },
+  plugins: [],
+}
