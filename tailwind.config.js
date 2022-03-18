ec04e0b8a1c12e90669429ae567310085c3dd7a2
janssens <janssensgaetan@gmail.com>
2022-03-18 22:30:20 +0100

    a good start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['nunito_sans', ...defaultTheme.fontFamily.sans],
+        'serif': ['libre_baskerville', ...defaultTheme.fontFamily.serif],
+      },
       colors: {
         'union-blue': '#3F2682',
         'eelv-green': '#409682'
