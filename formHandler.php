3ebdccde9729b7db3d19f9ec8182d49202e7684a
Gaetan Janssens <janssensgaetan@gmail.com>
2022-03-24 19:49:39 +0100

    frontend a bit, backend a lot

--- formHandler.php
+++ formHandler.php
@@ -1,21 +1,46 @@
 <?php
 
+
+
 class Entry{
+    /**
+     * @var DateTime
+     */
+    private $_created_at;
     private $_firstname;
     private $_lastname;
     private $_mail;
     private $_phone;
     private $_postcode;
+    /**
+     * @var boolean
+     */
+    private $_info;
+    /**
+     * @var boolean
+     */
     private $_help;
+    /**
+     * @var boolean
+     */
+    private $_join_us;
+    /**
+     * @var array
+     */
+    private $_extra;
 
     public function __construct()
     {
+        $this->_created_at = '';
         $this->_firstname = '';
         $this->_lastname = '';
         $this->_mail = '';
         $this->_phone = '';
         $this->_postcode = '';
+        $this->_info = false;
         $this->_help = false;
+        $this->_join_us = false;
+        $this->_extra = json_encode([]);
     }
 
     /**
@@ -23,12 +48,22 @@ class Entry{
      * @return $this
      */
     public function initFromPost($post){
+        $this->_created_at = new DateTime('now');
         $this->_firstname = $post['firstname'];
         $this->_lastname = $post['lastname'];
         $this->_mail = $post['mail'];
         $this->_phone = $post['telephone'];
         $this->_postcode = $post['postcode'];
+        $this->_info = $post['more_info'] == 'on';
         $this->_help = $post['help'] == 'on';
+        $this->_join_us = $post['join_us'] == 'on';
+        $extra = [];
+        foreach ($post as $key => $val){
+            if (!in_array($key,['firstname','lastname','mail','telephone','postcode','help','more_indo','join_us','redirect_path'])){
+                $extra[$key] = $val;
+            }
+        }
+        $this->_extra = $extra;
         return $this;
     }
 
@@ -37,12 +72,41 @@ class Entry{
      * @return $this
      */
     public function initFromRow($row){
+        $this->_created_at = DateTime::createFromFormat(DateTime::ATOM,$row[0]);
+        $this->_firstname = $row[1];
+        $this->_lastname = $row[2];
+        $this->_mail = $row[3];
+        $this->_phone = $row[4];
+        $this->_postcode = $row[5];
+        $this->_info = $row[6];
+        $this->_help = $row[7];
+        $this->_join_us = $row[8];
+        $this->_extra = json_decode($row[9]);
         return $this;
     }
 
     public function toArray(){
-        return [$this->getFirstname(),$this->getLastname()];
+        return [
+            $this->getCreatedAt()->format(DateTime::ATOM),
+            $this->getFirstname(),
+            $this->getLastname(),
+            $this->getMail(),
+            $this->getPhone(),
+            $this->getPostcode(),
+            $this->getInfo(),
+            $this->getHelp(),
+            $this->getJoinUs(),
+            json_encode($this->getExtra())
+            ];
     }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(){
+        return $this->_created_at;
+    }
+
 
     public function getFirstname(){
         return $this->_firstname;
@@ -50,6 +114,38 @@ class Entry{
 
     public function getLastname(){
         return $this->_lastname;
+    }
+
+    public function getMail(){
+        return $this->_mail;
+    }
+
+    public function getPhone(){
+        return $this->_phone;
+    }
+
+    public function getPostcode(){
+        return $this->_postcode;
+    }
+    
+    public function getInfo(){
+        return $this->_info;
+    }
+    
+    public function getHelp(){
+        return $this->_help;
+    }
+
+    public function getJoinUs(){
+        return $this->_join_us;
+    }
+
+    public function getExtra(){
+        return $this->_extra;
+    }
+
+    static function getHeader(){
+        return ["created_at","firstname","lastname",'mail','phone','postcode','info','help','join_us','extra'];
     }
 
     /**
@@ -75,7 +171,7 @@ class Entry{
         if ($post['rgpd']!='on')
             return false;
 
-        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
             $err = "Invalid email format";
             return false;
         }
@@ -83,39 +179,68 @@ class Entry{
     }
 }
 
-if (isset($_POST['name'])&&strlen($_POST['name'])){
-    return '';
-}else{
-    $filename = "data/data.csv";
-    $data = [];
-    if (file_exists($filename)){
-        //Open the file.
-        $fileHandle = fopen($filename, "r");
-        //Loop through the CSV rows.
-        while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-            //Dump out the row for the sake of clarity.
-            $data[] = Entry::fromRow($row);
-        }
-        fclose($fileHandle);
-        var_dump($data);
-        die();
+class EntryCsv{
+    private $_filename;
+    /**
+     * @var Entry[]
+     */
+    private $_entries;
+    private $_with_header;
+
+    public function __construct($_filename,$_with_header)
+    {
+        $this->_entries = [];
+        $this->_filename = $_filename;
+        $this->_with_header = $_with_header;
     }
 
+    public function addEntry(Entry $entry){
+        if (file_exists($this->_filename)){
+            //Open the file.
+            $fileHandle = fopen($this->_filename, "r");
+            $i = 0;
+            //Loop through the CSV rows.
+            while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
+                if ($i++===0 && $this->_with_header){
+                    continue;
+                }
+                //Dump out the row for the sake of clarity.
+                $this->_entries[] = Entry::fromRow($row);
+            }
+            fclose($fileHandle);
+        }
+        $this->_entries[] = $entry;
+        // open csv file for writing
+        $fileHandle = fopen($this->_filename, 'w');
+        if ($fileHandle === false) {
+            die('Error opening the file ' . $this->_filename);
+        }
+        if ($this->_with_header){
+            fputcsv($fileHandle, Entry::getHeader());
+        }
+        // write each row at a time to a file
+        foreach ($this->_entries as $entry) {
+            fputcsv($fileHandle, $entry->toArray());
+        }
+        // close the file
+        fclose($fileHandle);
+    }
+}
+
+if (isset($_POST['name'])&&strlen($_POST['name'])){
+    //honey pot
+}else{
+    $filename = "data/data.csv";
     if (Entry::checkPost($_POST)){
         $entry = new Entry();
         $entry->initFromPost($_POST);
-        $data[] = $entry;
+        $csv = new EntryCsv($filename,true);
+        $csv->addEntry($entry);
     }
-
-    // open csv file for writing
-    $fileHandle = fopen($filename, 'w');
-    if ($fileHandle === false) {
-        die('Error opening the file ' . $filename);
-    }
-    // write each row at a time to a file
-    foreach ($data as $entry) {
-        fputcsv($fileHandle, $entry->toArray());
-    }
-    // close the file
-    fclose($fileHandle);
 }
+
+if (isset($_POST['redirect_path']))
+    header('Location: '. $_SERVER['HTTP_HOST'].$_POST['redirect_path']);
+else
+    header('Location: /');
+die();
